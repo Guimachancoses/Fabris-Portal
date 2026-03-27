b741051e691812d09a19b3764a911c273ae66052
Guilherme Machancoses <guilherme.machancoses@gmail.com>
2026-03-27 18:15:56 -0300

    api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "http://ec2-3-147-238-182.us-east-2.compute.amazonaws.com:8000",
+  // baseURL: "http://localhost:8080",
+  baseURL: "http://18.224.5.103:8080",
 });
 
 export default api;
