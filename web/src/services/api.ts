8674161411be282edcc2fdec8c8caaea1202ada1
Guilherme Machancoses <guilherme.machancoses@gmail.com>
2026-03-30 10:46:00 -0300

    ok

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://18.224.5.103:8080",
+  baseURL: "https://api.fabrisportalhub.com.br",
 });
 
 export default api;
